c4856c4a11904a9eb3b803f674c4eb83d963260f
fargitgit <afarheen751@gmail.com>
2022-02-28 22:02:53 +0530

    weather api

---
+++ src/Weather/ShowTemp.jsx
@@ -0,0 +1,12 @@
+
+const ShowTemp = ({temp})  =>{
+    return (
+        <>  
+            <div className="show_temp">
+                <p>Temperature<span>{temp}</span></p>
+            </div>
+
+        </>
+    )
+}
+export  default ShowTemp;
---
+++ src/Weather/Weather_main.jsx
@@ -0,0 +1,12 @@
+
+import Clouds from './Clouds';
+import Temp from './Temp';
+const Weather_main = () =>{
+    return (
+        <>
+            <Clouds addstyle="leftClass" />
+            <Temp />
+        </>
+    )
+}
+export default Weather_main;
